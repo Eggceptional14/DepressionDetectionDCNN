95b98e5194def011ce1aa6b4e4348fe4441e5e7a
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2024-07-01 22:53:27 +0100

    [feat]: update Dataset class

--- src/Dataset.py
+++ src/Dataset.py
@@ -1,12 +1,17 @@
 from torch.utils.data import Dataset
+import pandas as pd
 
 
 class FacialDataset(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, feature_file, label_file=None, is_train=True):
+        self.visual_feature = pd.read_csv(feature_file)
+        self.labels = None
+        if label_file:
+            self.labels = pd.read_csv()
+        self.is_train = is_train
 
     def __len__(self):
-        pass
+        return len(self.visual_feature)
 
     def __getitem__(self):
         pass
