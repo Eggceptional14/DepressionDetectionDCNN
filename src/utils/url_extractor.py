5d81945cd32f69ea25c07d6a7712b0fbd391cb45
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2024-07-12 20:36:59 +0100

    feat: update url getter function

--- src/utils/url_extractor.py
+++ src/utils/url_extractor.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_file_url():
-    data_url = os.getenv("EDAIC_DATA_URL")
+def get_file_url(file_extension):
+    data_url = os.getenv("DAIC_DATA_URL")
     response = requests.get(data_url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -15,8 +15,11 @@ def get_file_url():
     # Retrieve file url from webserver
     for link in soup.find_all('a'):
         file_url = link.get('href')
-        if file_url.endswith('.tar.gz'):
+        if file_url.endswith(f'_P{file_extension}'):
             # print(file_url)
             urls.append(file_url)
 
     return urls
+
+if __name__ == '__main__':
+    print(get_file_url('.zip'))
