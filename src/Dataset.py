294fa35cd47359bb72124165bf2dafb048fd47a2
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2024-07-12 21:22:14 +0100

    feat: update dataset class

--- src/Dataset.py
+++ src/Dataset.py
@@ -3,15 +3,18 @@ import pandas as pd
 
 
 class DAICDataset(Dataset):
-    def __init__(self, feature_file, label_file=None, is_train=True):
-        self.visual_feature = pd.read_csv(feature_file)
+    def __init__(self, landmark_file, au_file, gaze_file, label_file=None, is_train=True):
+        self.landmarks = pd.read_csv(landmark_file)
+        self.aus = pd.read_csv(au_file)
+        self.gaze = pd.read_csv(gaze_file)
+
         self.labels = None
         if label_file:
-            self.labels = pd.read_csv()
+            self.labels = pd.read_csv(label_file)
         self.is_train = is_train
 
     def __len__(self):
-        return len(self.visual_feature)
+        return (len(self.landmarks), len(self.aus), len(self.gaze))
 
     def __getitem__(self):
         pass
