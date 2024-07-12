a4426d97f583c34de1aeff7bdb70028fca13ce1e
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2024-07-12 21:22:05 +0100

    feat: update file downloader pipeline

--- src/utils/downloader.py
+++ src/utils/downloader.py
@@ -1,6 +1,7 @@
 import requests
 import io
 import tarfile
+import zipfile
 import os
 from dotenv import load_dotenv
 
@@ -12,17 +13,36 @@ def file_processor(file_url):
     response = requests.get(file_url)
     data = io.BytesIO(response.content)
 
+    if file_url.endswith('.tar.gz'):
+        extracted_data = tar_processor(data)
+    elif file_url.endswith('.zip'):
+        extracted_data = zip_processor(data)
+    else:
+        raise ValueError('Unsupported file format')
+
+    return extracted_data
+
+def tar_processor(data):
     # extract content of tar file
     with tarfile.open(fileobj=data, mode='r:gz') as tar:
         members = tar.getmembers()
-        extracted_data = {
+        extracted_files = {
             member.name: tar.extractfile(member).read()
             for member in members
             if member.isfile()
         }
+    return extracted_files
 
-    return extracted_data
+def zip_processor(data):
+    # extract content of zip file
+    with zipfile.ZipFile(data, 'r') as zip:
+        extracted_files = {
+            member: zip.read(member)
+            for member in zip.namelist()
+            if 'CLNF' in member and member.endswith('.txt')
+        }
 
+    return extracted_files
 
 if __name__ == '__main__':
-    print(file_processor(os.getenv("TEST_URL")))
+    print(file_processor(os.getenv("DAIC_TEST_URL")))
--- src/utils/train.py
+++ src/utils/train.py
@@ -18,11 +18,19 @@ def train():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
 
-    base_url = os.getenv('EDAIC_DATA_URL')
-    file_urls = get_file_url()
+    file_extension = '.zip'
+    base_url = os.getenv('DAIC_DATA_URL')
+    file_urls = get_file_url(file_extension)
 
     # loop through all files
     for file_url in file_urls:
         # load data into dataset class
         data = file_processor(base_url + file_url)
-        dataset = DAICDataset(io.ByteIO(data.get()))
+        landmarks = io.ByteIO(data.get(f'{file_url[:4]}CLNF_features.txt'))
+        aus = io.ByteIO(data.get(f'{file_url[:4]}CLNF_AUs.txt'))
+        gaze = io.ByteIO(data.get(f'{file_url[:4]}CLNF_gaze.txt'))
+        dataset = DAICDataset(landmarks, 
+                              aus, 
+                              gaze, 
+                              label_file='data/daic/train_split_Depression_AVEC2017.csv',
+                              is_train=True)
