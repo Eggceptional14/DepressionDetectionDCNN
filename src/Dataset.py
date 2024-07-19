dc907c359eb29e5c6e1227aca1c4aa8a2c16ef63
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2024-07-19 15:36:39 +0100

    feat: update getitem function

--- src/Dataset.py
+++ src/Dataset.py
@@ -11,38 +11,44 @@ class DAICDataset(Dataset):
         self.gaze = pd.read_csv(gaze_file)
 
         self.labels = None
-        if label_file:
+        if type(label_file) == str:
             self.labels = pd.read_csv(label_file)
+        elif type(label_file) == pd.core.frame.DataFrame:
+            self.labels = label_file
         self.is_train = is_train
 
     def __len__(self):
         return len(self.landmarks) # return frame length. All of the features have the same length
 
     def __getitem__(self, idx):
-        landmarks = self.landmarks[idx]
-        aus = self.aus[idx]
-        gaze = self.gaze[idx]
+        landmarks = self.landmarks.iloc[idx, 4:]
+        aus = self.aus.iloc[idx, 4:]
+        gaze = self.gaze.iloc[idx, 4:]
 
         return (
-            {"frame": idx, "landmarks": landmarks, "aus": aus, "gaze": gaze, "labels": self.labels[self.pid]}
+            {"frame": idx, 
+             "landmarks": landmarks, 
+             "aus": aus, 
+             "gaze": gaze, 
+             "labels": self.labels[self.labels['Participant_ID'] == self.pid]}
             if self.is_train
             else {"frame": idx, "landmarks": landmarks, "aus": aus, "gaze": gaze}
         )
     
-    def __getitem__(self, idx):
-        seq_id = self.sequences['PDB_ID'].iloc[idx]
-        seq = self.sequences['SEQUENCE'].iloc[idx]
+    # def __getitem__(self, idx):
+    #     seq_id = self.sequences['PDB_ID'].iloc[idx]
+    #     seq = self.sequences['SEQUENCE'].iloc[idx]
         
-        if self.is_train:
-            pssm_df = pd.read_csv(f"{self.pssm_dir}/{seq_id}_train.csv")
-        else:
-            pssm_df = pd.read_csv(f"{self.pssm_dir}/{seq_id}_test.csv")
-        pssm_values = pssm_df.iloc[:, 2:].values
-        pssm = torch.tensor(pssm_values, dtype=torch.float32)
-        sample = {'seq': seq, 'pssm': pssm}
+    #     if self.is_train:
+    #         pssm_df = pd.read_csv(f"{self.pssm_dir}/{seq_id}_train.csv")
+    #     else:
+    #         pssm_df = pd.read_csv(f"{self.pssm_dir}/{seq_id}_test.csv")
+    #     pssm_values = pssm_df.iloc[:, 2:].values
+    #     pssm = torch.tensor(pssm_values, dtype=torch.float32)
+    #     sample = {'seq': seq, 'pssm': pssm}
 
-        if self.label_data is not None:
-            labels = self.label_data[self.label_data['PDB_ID'] == seq_id]['SEC_STRUCT'].values[0]
-            sample['labels'] = labels
+    #     if self.label_data is not None:
+    #         labels = self.label_data[self.label_data['PDB_ID'] == seq_id]['SEC_STRUCT'].values[0]
+    #         sample['labels'] = labels
 
-        return sample
+    #     return sample
