d887da97a12206c83cfb450c031e7f7fcc5ea2ff
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2024-07-22 18:57:01 +0100

    feat: update DAICDataset class

--- src/Dataset.py
+++ src/Dataset.py
@@ -1,54 +1,49 @@
+import os
+import io
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
+from dotenv import load_dotenv
+
+from utils.downloader import file_processor
+
+load_dotenv()
 
 
 class DAICDataset(Dataset):
-    def __init__(self, pid, landmark_file, au_file, gaze_file, label_file=None, is_train=True):
-        self.pid =pid
-        self.landmarks = pd.read_csv(landmark_file)
-        self.aus = pd.read_csv(au_file)
-        self.gaze = pd.read_csv(gaze_file)
 
-        self.labels = None
-        if type(label_file) == str:
-            self.labels = pd.read_csv(label_file)
-        elif type(label_file) == pd.core.frame.DataFrame:
-            self.labels = label_file
+    def __init__(self, split_details, is_train=True):
+        self.split_details = pd.read_csv(split_details) # read split of DAIC dataset (contains pid and labels)
+        self.data_url = os.getenv('DAIC_DATA_URL')
         self.is_train = is_train
 
     def __len__(self):
-        return len(self.landmarks) # return frame length. All of the features have the same length
+        return len(self.split_details)
 
     def __getitem__(self, idx):
-        landmarks = self.landmarks.iloc[idx, 4:]
-        aus = self.aus.iloc[idx, 4:]
-        gaze = self.gaze.iloc[idx, 4:]
+        pid = self.split_details['Participant_ID'].iloc[idx]
 
-        return (
-            {"frame": idx, 
-             "landmarks": landmarks, 
-             "aus": aus, 
-             "gaze": gaze, 
-             "labels": self.labels[self.labels['Participant_ID'] == self.pid]}
-            if self.is_train
-            else {"frame": idx, "landmarks": landmarks, "aus": aus, "gaze": gaze}
-        )
-    
-    # def __getitem__(self, idx):
-    #     seq_id = self.sequences['PDB_ID'].iloc[idx]
-    #     seq = self.sequences['SEQUENCE'].iloc[idx]
+        file_url = f"{str(pid)}_P.zip"
+        data = file_processor(self.data_url + file_url)
+        print(f'##### Finish Downloading File: {file_url} #####\n')
+
+        landmarks = pd.read_csv(io.BytesIO(data.get(f'{pid}_CLNF_features.txt')))
+        aus = pd.read_csv(io.BytesIO(data.get(f'{pid}_CLNF_AUs.txt')))
+        gaze = pd.read_csv(io.BytesIO(data.get(f'{pid}_CLNF_gaze.txt')))
+
+        landmarks = landmarks[landmarks[' success'] == 1].iloc[:, 4:]
+        aus = aus[aus[' success'] == 1].iloc[:, 4:]
+        gaze = gaze[gaze[' success'] == 1].iloc[:, 4:]
         
-    #     if self.is_train:
-    #         pssm_df = pd.read_csv(f"{self.pssm_dir}/{seq_id}_train.csv")
-    #     else:
-    #         pssm_df = pd.read_csv(f"{self.pssm_dir}/{seq_id}_test.csv")
-    #     pssm_values = pssm_df.iloc[:, 2:].values
-    #     pssm = torch.tensor(pssm_values, dtype=torch.float32)
-    #     sample = {'seq': seq, 'pssm': pssm}
+        sample = {
+            'pid': pid,
+            'landmarks': torch.tensor(landmarks, dtype=torch.float32),
+            'aus': torch.tensor(aus, dtype=torch.float32),
+            'gaze': torch.tensor(gaze, dtype=torch.float32)
+        }
 
-    #     if self.label_data is not None:
-    #         labels = self.label_data[self.label_data['PDB_ID'] == seq_id]['SEC_STRUCT'].values[0]
-    #         sample['labels'] = labels
+        if self.is_train:
+            label = self.split_details[self.split_details['Participant_ID'] == pid]['PHQ8_Binary'].values[0]
+            sample["label"] = label
 
-    #     return sample
+        return sample
