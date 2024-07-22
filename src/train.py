8f9d374a727e7ad4d3e20900c8b28e714282b51a
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2024-07-22 19:49:11 +0100

    feat: update model training function

--- src/train.py
+++ src/train.py
@@ -1,61 +1,91 @@
-import torch
-import os
-import io
-import requests
 import pandas as pd
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+
+import torch
+import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
+import ray
+import ray.cloudpickle as pickle
+from ray import train,tune
+from ray.train import Checkpoint
+from ray.tune.schedulers import ASHAScheduler
+
 from Model import CNNModel, LSTMModel, MultiModalModel
 from Dataset import DAICDataset
-from utils.downloader import file_processor
-from utils.url_extractor import get_file_url
 
 load_dotenv()
 
 
 def train():
+    # use the same config as the previous work
     config = {
-        "dropout": tune.loguniform(0.2, 0.4),
-        "lr": tune.loguniform(1e-4, 1e-1),
-        "batch_size": tune.choice([16, 32, 64]),
+        "dropout": 0.2,
+        "lr": 0.001,
+        "batch_size": 1,
+        "hidden_size": 32
     }
-    model = MultiModalModel()
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = LSTMModel(input_size=136, hidden_size=config['hidden_size'], output_size=1, device=device)
     model.to(device)
 
-    optim = optim.Adam(model.parameters())
+    optimizer = optim.Adam(model.parameters(), lr=config['lr'])
+    criterion = nn.CrossEntropyLoss()
 
-    file_extension = '.zip'
-    base_url = os.getenv('DAIC_DATA_URL')
-    # file_urls = get_file_url(file_extension)
-    train_labels = pd.read_csv("data/daic/train_split_Depression_AVEC2017.csv")
+    train_labels_dir = "data/daic/train_split_Depression_AVEC2017.csv"
+    val_labels_dir = "data/daic/dev_split_Depression_AVEC2017.csv"
+
+    train_dataset = DAICDataset(train_labels_dir, is_train=True)
+    val_dataset = DAICDataset(val_labels_dir, is_train=True)
+
+    train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=config['batch_size'], shuffle=True)
+
     print("##### Model Training #####\n\n")
 
-    # loop through all files
-    for pid in train_labels['Participant_ID']:
-        # load data into dataset class
-        file_url = f"{str(pid)}_P.zip"
-        data = file_processor(base_url + file_url)
-        print(f'##### Finish Downloading File: {file_url} #####\n\n')
+    epochs = 1
+    for _ in range(epochs):
+        model.train()
+        
+        # Training loop
+        for batch in train_loader:
+            print("##### Start Training #####")
+            pid, landmarks, aus, gaze, label = batch['pid'], batch['landmarks'].to(device), batch['aus'].to(device), batch['gaze'].to(device), batch['label'].to(device)
+            optimizer.zero_grad()
+            output = model(landmarks)
+            loss = criterion(output, label)
+            loss.backward()
+            optimizer.step()
+            print(f'##### {config['batch_size']} Batch Finish #####\n')
+            
+        print('##### Training Complete, Starting Evaluation #####\n')
 
-        landmarks = io.BytesIO(data.get(f'{pid}_CLNF_features.txt'))
-        aus = io.BytesIO(data.get(f'{pid}_CLNF_AUs.txt'))
-        gaze = io.BytesIO(data.get(f'{pid}_CLNF_gaze.txt'))
+        # Evaluation loop
+        model.eval()
+        val_loss = 0.0
+        total_correct = 0
+        total_elements = 0
 
-        dataset = DAICDataset(pid,
-                              landmarks, 
-                              aus, 
-                              gaze, 
-                              label_file=train_labels,
-                              is_train=True)
+        with torch.no_grad():
+            for batch in val_loader:
+                print("##### Start Evaluation #####")
+                pid, landmarks, aus, gaze, label = batch['pid'], batch['landmarks'].to(device), batch['aus'].to(device), batch['gaze'].to(device), batch['label'].to(device)
+                output = model(landmarks)
+                val_loss += criterion(output, label).item()
+                _, predicted = torch.max(output.data, 1)
+                total_correct += (predicted == label).sum().item()
+                total_elements += label.nelement()
+                print(f'##### {config['batch_size']} Batch Finish #####\n')
 
-        print(f'Number of Frames in Dataset: {dataset.__len__()}')
-        print(dataset.__getitem__(0))
+        val_loss /= len(val_loader)
+        accuracy = total_correct / total_elements
 
-        break
+        print(f'Validation Loss: {val_loss} | Accuracy: {accuracy}')
+
+    print('##### Training complete #####\n\n')
+
         
 if __name__ == "__main__":
     train()
