2fe3dc661696d17bf961f097bb3e27a4012216f9
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2024-08-05 15:50:56 +0100

    .

--- src/train.py
+++ src/train.py
@@ -24,25 +24,38 @@ def train_model(config):
     landmarks_size = 136
     aus_size = 20
     gaze_size = 12
+    m_name = "lstm_aus"
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = LSTMModel(input_size=aus_size, hidden_size=config['hidden_size'], output_size=2, device=device)
+    model = LSTMModel(input_size=aus_size, 
+                      hidden_size=config['hidden_size'], 
+                      dropout_prob=config['dropout'], 
+                      output_size=2, 
+                      device=device)
+    # model = CNNModel(input_channels=aus_size, 
+    #                  hidden_size=config['hidden_size'], 
+    #                  output_size=2, 
+    #                  dropout=config['dropout'])
+    # model = MultiModalModel(ip_size_landmarks=landmarks_size,
+    #                         ip_size_aus=aus_size,
+    #                         ip_size_gaze=gaze_size,
+    #                         hidden_size=config['hidden_size'],
+    #                         output_size=2,
+    #                         device=device)
     model.to(device)
 
-    # train_labels_dir = "data/daic/train_split_Depression_AVEC2017.csv"
-    # val_labels_dir = "data/daic/dev_split_Depression_AVEC2017.csv"
     train_labels_dir = "/Users/pitchakorn/Dissertation/data/daic/train_split_Depression_AVEC2017.csv"
     val_labels_dir = "/Users/pitchakorn/Dissertation/data/daic/dev_split_Depression_AVEC2017.csv"
 
-    train_dataset = DAICDataset(train_labels_dir, is_train=True)
-    val_dataset = DAICDataset(val_labels_dir, is_train=True)
+    train_dataset = DAICDataset(train_labels_dir, config['frame_step'], is_train=True)
+    val_dataset = DAICDataset(val_labels_dir, config['frame_step'], is_train=True)
 
     train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=config['batch_size'], shuffle=True)
 
     if checkpoint := train.get_checkpoint():
         with checkpoint.as_directory() as checkpoint_dir:
-          with open(os.path.join(checkpoint_dir, 'data.pkl'), 'rb') as fp:
+          with open(os.path.join(checkpoint_dir, f'data_{m_name}.pkl'), 'rb') as fp:
             checkpoint_state = pickle.load(fp)
             start_epoch = checkpoint_state["epoch"]
             model.load_state_dict(checkpoint_state["net_state_dict"])
@@ -52,11 +65,9 @@ def train_model(config):
         optimizer = optim.Adam(model.parameters(), lr=config['lr'])
 
     criterion = nn.CrossEntropyLoss()
-
     # print("##### Model Training #####\n\n")
 
-    epochs = 10
-    for epoch in range(start_epoch, epochs):
+    for epoch in range(start_epoch, config["epochs"]):
         model.train()
 
         # Training loop
@@ -65,6 +76,7 @@ def train_model(config):
             pid, landmarks, aus, gaze, label = batch['pid'], batch['landmarks'].to(device), batch['aus'].to(device), batch['gaze'].to(device), batch['label'].to(device)
             optimizer.zero_grad()
             output, attention_weights = model(aus)
+            # output, _ = model(landmarks, aus, gaze)
             loss = criterion(output, label)
             loss.backward()
             optimizer.step()
@@ -83,10 +95,12 @@ def train_model(config):
                 # print("##### Start Evaluation #####")
                 pid, landmarks, aus, gaze, label = batch['pid'], batch['landmarks'].to(device), batch['aus'].to(device), batch['gaze'].to(device), batch['label'].to(device)
                 output, attention_weights = model(aus)
+                # output, _ = model(landmarks, aus, gaze)
                 val_loss += criterion(output, label).item()
                 _, predicted = torch.max(output.data, 1)
                 total_correct += (predicted == label).sum().item()
-                total_elements += label.nelement()
+                # total_elements += label.nelement()
+                total_elements += label.size(0)
                 # print(f'##### {config['batch_size']} Batch Finish #####\n')
 
         val_loss /= len(val_loader)
@@ -101,54 +115,14 @@ def train_model(config):
         }
 
         with tempfile.TemporaryDirectory() as checkpoint_dir:
-          with open(os.path.join(checkpoint_dir, 'data.pkl'), 'wb') as fp:
-            pickle.dump(checkpoint_data, fp)
+            with open(os.path.join(checkpoint_dir, f'data_{m_name}.pkl'), 'wb') as fp:
+                pickle.dump(checkpoint_data, fp)
 
-          checkpoint = Checkpoint.from_directory(checkpoint_dir)
+            checkpoint = Checkpoint.from_directory(checkpoint_dir)
 
-          train.report(
-              {"loss": val_loss, "accuracy": accuracy},
-              checkpoint=checkpoint,
-          )
+            train.report(
+                {"loss": val_loss, "accuracy": accuracy},
+                checkpoint=checkpoint,
+            )
 
     # print('##### Training complete #####\n\n')
-
-if __name__ == "__main__":
-    config = {
-        "dropout": 0.2,
-        "lr": 0.001,
-        "batch_size": 1,
-        "hidden_size": 32
-    }
-
-    scheduler = ASHAScheduler(
-        metric="loss",
-        mode="min",
-        max_t=10,
-        grace_period=1,
-        reduction_factor=2,
-    )
-
-    result = tune.run(
-            train_model,
-            resources_per_trial={"cpu": 8, "gpu": 0},
-            config=config,
-            num_samples=1,
-            scheduler=scheduler,
-    )
-
-    best_trial = result.get_best_trial("loss", "min", "last")
-    print(f"Best trial config: {best_trial.config}")
-    print(f"Best trial final validation loss: {best_trial.last_result['loss']}")
-    print(f"Best trial final validation accuracy: {best_trial.last_result['accuracy']}")
-
-    # save best model
-    best_checkpoint = best_trial.checkpoint
-
-    with best_checkpoint.as_directory() as checkpoint_dir:
-        best_model_state = torch.load(os.path.join(checkpoint_dir, 'data.pkl'))
-
-    model = LSTMModel(input_size=136, hidden_size=best_trial.config['hidden_size'], output_size=2, device="cuda" if torch.cuda.is_available() else "cpu")
-    model.load_state_dict(best_model_state["net_state_dict"])
-
-    torch.save(model.state_dict(), "best_model.pth")
