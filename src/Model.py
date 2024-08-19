852befd5a8433d4b5c1ae17bf3009fedbaefcd79
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2024-08-19 04:24:39 +0100

    :recycle: refactor: clean up models code

--- src/Model.py
+++ src/Model.py
@@ -1,20 +1,51 @@
 import torch
 import torch.nn as nn
 
-from Attention import Attention
 
+class Attention(nn.Module):
+    def __init__(self, hidden_dim, num_heads):
+        super(Attention, self).__init__()
+        self.attention = nn.MultiheadAttention(embed_dim=hidden_dim, num_heads=num_heads, batch_first=True)
 
-class CNNModel(nn.Module):
-    def __init__(self, input_channels, hidden_size, output_size, dropout):
+    def forward(self, x):
+        output, attention_weights = self.attention(x, x, x)
+        return output, attention_weights
+
+class LSTMModule(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, dropout_prob, device):
+        super(LSTMModule, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.device = device
+        
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.attention = Attention(hidden_size, 4)
+        self.dropout = nn.Dropout(dropout_prob)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        
+        out, _ = self.lstm(x, (h0, c0))
+        out, attention_weights = self.attention(out)
+
+        out = torch.mean(out, dim=1)
+
+        out = self.dropout(out)
+        
+        return out, attention_weights
+
+class CNNModule(nn.Module):
+    def __init__(self, input_channels, dropout):
         super(CNNModel, self).__init__()
 
         self.conv1 = nn.Conv1d(input_channels, 64, kernel_size=3, padding=1)
         self.conv2 = nn.Conv1d(64, 128, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv1d(128, hidden_size, kernel_size=3, padding=1)
-        
-        self.pool1 = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
-        self.pool2 = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
-        self.pool3 = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
+        self.conv3 = nn.Conv1d(128, 256, kernel_size=3, padding=1)
+
+        self.pool1 = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.pool2 = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.pool3 = nn.MaxPool1d(kernel_size=2, stride=2)
 
         self.dropout = nn.Dropout(p=dropout)
 
@@ -22,28 +53,27 @@ class CNNModel(nn.Module):
         self.relu2 = nn.ReLU()
         self.relu3 = nn.ReLU()
 
-        self.attention = Attention(hidden_size, 1)
+        self.attention = Attention(256, 4)
         self.global_avg_pool = nn.AdaptiveAvgPool1d(1)
-
-        self.fc = nn.Sequential(
-            nn.Linear(hidden_size , 64),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(32, output_size),
-        )
 
     def forward(self, x):
         x = x.permute(0, 2, 1)
-        x = self.pool1(self.relu1(self.conv1(x)))
+
+        x = self.relu1(self.conv1(x))
+        # print('Shape before pool', 1, x.shape)
+        x = self.pool1(x)
         x = self.dropout(x)
-        x = self.pool2(self.relu2(self.conv2(x)))
+
+        x = self.relu2(self.conv2(x))
+        # print('Shape before pool', 2, x.shape)
+        x = self.pool2(x)
         x = self.dropout(x)
-        x = self.pool3(self.relu3(self.conv3(x)))
+
+        x = self.relu3(self.conv3(x))
+        # print('Shape before pool', 3, x.shape)
+        x = self.pool3(x)
         x = self.dropout(x)
-        
+
         x = x.permute(0, 2, 1)
         x, attention_weights = self.attention(x)
 
@@ -51,99 +81,74 @@ class CNNModel(nn.Module):
         x = self.global_avg_pool(x)
         x = x.squeeze(-1)
 
-        x = self.fc(x)
-        x = torch.sigmoid(x)
         return x, attention_weights
-class LSTMModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, device, dropout_prob, num_layers=1):
-        super(LSTMModel, self).__init__()
-        self.num_layers = num_layers
-        self.hidden_size = hidden_size
-        self.device = device
 
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+class CNNModel(nn.Module):
+    def __init__(self, input_channels, output_size, dropout):
+        super(CNNModel, self).__init__()
 
-        self.attention = Attention(hidden_size, 1)
-
-        # self.fc = nn.Linear(hidden_size, output_size)
-
-        self.dropout = nn.Dropout(dropout_prob)
+        self.cnn_module = CNNModule(input_channels, dropout)
 
         self.fc = nn.Sequential(
-            nn.Linear(hidden_size, 64),
+            nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Dropout(dropout_prob),
-            nn.Linear(64, 32),
+            nn.Dropout(dropout),
+            nn.Linear(128, 64),
             nn.ReLU(),
-            nn.Dropout(dropout_prob),
-            nn.Linear(32, output_size),
+            nn.Dropout(dropout),
+            nn.Linear(64, output_size),
         )
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
-        out, _ = self.lstm(x, (h0, c0))
-        out, attention_weights = self.attention(out)
-        out = out[:, -1, :]
-        out = self.dropout(out)
-        out = self.fc(out)
-        out = torch.sigmoid(out)
-        return out, attention_weights
-    
+        x, attention_weights = self.cnn_module(x)
+        x = self.fc(x)
+        return x, attention_weights
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, device, dropout_prob, num_layers=1):
+        super(LSTMModel, self).__init__()
+
+        self.lstm_module = LSTMModule(input_size, hidden_size, num_layers, dropout_prob, device)
+
+        self.fc = nn.Sequential(
+            nn.Linear(hidden_size, 128),
+            nn.ReLU(),
+            nn.Dropout(dropout_prob),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(dropout_prob),
+            nn.Linear(64, output_size),
+        )
+
+    def forward(self, x):
+        x, attention_weights = self.lstm_module(x)
+        x = self.fc(x)
+        return x, attention_weights
+
 class MultiModalModel(nn.Module):
     def __init__(self, ip_size_landmarks, ip_size_aus, ip_size_gaze, hidden_size, output_size, device, dropout_prob, num_layers=1):
         super(MultiModalModel, self).__init__()
-        self.num_layers = num_layers
-        self.hidden_size = hidden_size
-        self.device = device
-
-        self.lstm_landmarks = nn.LSTM(ip_size_landmarks, hidden_size, num_layers, batch_first=True)
-        self.lstm_aus = nn.LSTM(ip_size_aus, hidden_size, num_layers, batch_first=True)
-        self.lstm_gaze = nn.LSTM(ip_size_gaze, hidden_size, num_layers, batch_first=True)
-
-        self.attention_landmarks = Attention(hidden_size, 1)
-        self.attention_aus = Attention(hidden_size, 1)
-        self.attention_gaze = Attention(hidden_size, 1)
-
-        self.dropout = nn.Dropout(dropout_prob)
+        
+        self.landmarks_lstm = LSTMModule(ip_size_landmarks, hidden_size, num_layers, dropout_prob, device)
+        self.aus_lstm = LSTMModule(ip_size_aus, hidden_size, num_layers, dropout_prob, device)
+        self.gaze_lstm = LSTMModule(ip_size_gaze, hidden_size, num_layers, dropout_prob, device)
 
         self.fc = nn.Sequential(
-            nn.Linear(hidden_size, 64),
+            nn.Linear(hidden_size * 3, 128),
             nn.ReLU(),
             nn.Dropout(dropout_prob),
-            nn.Linear(64, 32),
+            nn.Linear(128, 64),
             nn.ReLU(),
             nn.Dropout(dropout_prob),
-            nn.Linear(32, output_size),
+            nn.Linear(64, output_size),
         )
 
     def forward(self, landmarks, aus, gaze):
-        h0_landmarks = torch.zeros(self.num_layers, landmarks.size(0), self.hidden_size).to(self.device)
-        c0_landmarks = torch.zeros(self.num_layers, landmarks.size(0), self.hidden_size).to(self.device)
-        h0_aus = torch.zeros(self.num_layers, aus.size(0), self.hidden_size).to(self.device)
-        c0_aus = torch.zeros(self.num_layers, aus.size(0), self.hidden_size).to(self.device)
-        h0_gaze = torch.zeros(self.num_layers, gaze.size(0), self.hidden_size).to(self.device)
-        c0_gaze = torch.zeros(self.num_layers, gaze.size(0), self.hidden_size).to(self.device)
-
-        # Process each modality through its LSTM
-        out_landmarks, _ = self.lstm_landmarks(landmarks, (h0_landmarks, c0_landmarks))
-        out_aus, _ = self.lstm_aus(aus, (h0_aus, c0_aus))
-        out_gaze, _ = self.lstm_gaze(gaze, (h0_gaze, c0_gaze))
-
-        out_landmarks, attn_weights_landmarks = self.attention_landmarks(out_landmarks)
-        out_aus, attn_weights_aus = self.attention_aus(out_aus)
-        out_gaze, attn_weights_gaze = self.attention_gaze(out_gaze)
-
-        out_landmarks = out_landmarks[:, -1, :]
-        out_aus = out_aus[:, -1, :]
-        out_gaze = out_gaze[:, -1, :]
-
-        out_landmarks = self.dropout(out_landmarks)
-        out_aus = self.dropout(out_aus)
-        out_gaze = self.dropout(out_gaze)
+        out_landmarks, attn_weights_landmarks = self.landmarks_lstm(landmarks)
+        out_aus, attn_weights_aus = self.aus_lstm(aus)
+        out_gaze, attn_weights_gaze = self.gaze_lstm(gaze)
 
         combined_out = torch.cat((out_landmarks, out_aus, out_gaze), dim=1)
         out = self.fc(combined_out)
-        out = torch.sigmoid(out)
-
-        return out_gaze, (attn_weights_landmarks, attn_weights_aus, attn_weights_gaze)
+        
+        return out, (attn_weights_landmarks, attn_weights_aus, attn_weights_gaze)
